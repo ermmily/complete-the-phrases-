7957458461bd2b81d62294d153ebe25d6a652e18
ermmily <146213261+ermmily@users.noreply.github.com>
2023-11-20 10:09:45 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+// program Title
+
+console.log("hello, world!");
+
+document.getElementById("leg").addEventListener("click", function1);
+document.getElementById("devil").addEventListener("click", function2);
+document.getElementById("worse").addEventListener("click", function3);
+document.getElementById("square").addEventListener("click", function4);
+document.getElementById("rktSci").addEventListener("click", function5);
+// input
+function function1() {
+  alert("Break a leg");
+}
+function function2() {
+  alert("Speak of the devil");
+}
+function function3() {
+  alert("To make matters worse");
+}
+function function4() {
+  alert("Fair and square");
+}
+function function5() {
+  alert("It's not rocket science");
+}
+//ouput
